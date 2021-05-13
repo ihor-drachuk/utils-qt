8f513c0b5b0b76d62f17cc6b987651b0d7f7ecb0
AlexVyznyuk <alexander.vyznyuk@developex.net>
2021-05-13 12:42:19 +0300

    Added NumericalValidator

---
+++ headers/utils-qt/NumericalValidator.h
@@ -0,0 +1,53 @@
+#pragma once
+#include <QValidator>
+#include <QVariant>
+#include <utils-cpp/pimpl.h>
+
+class NumericalValidator : public QValidator {
+    Q_OBJECT
+
+    Q_PROPERTY(QVariant step READ step WRITE setStep NOTIFY stepChanged)
+    Q_PROPERTY(QVariant top READ top WRITE setTop NOTIFY topChanged)
+    Q_PROPERTY(QVariant bottom READ bottom WRITE setBottom NOTIFY bottomChanged)
+
+public:
+    static void registerTypes(const char* url);
+
+    enum ValueRangeStatus {
+        TopValue = 0,
+        BottomValue,
+        IntermediateValue
+    };
+
+    Q_ENUM(ValueRangeStatus)
+
+    NumericalValidator(QObject* parent = nullptr);
+    ~NumericalValidator() override;
+
+    Q_INVOKABLE ValueRangeStatus isValue(const QString& input);
+
+    State validate(QString& input, int& pos) const override;
+    void fixup(QString& input) const override;
+
+    QVariant step() const;
+    QVariant bottom() const;
+    QVariant top() const;
+
+public slots:
+    void setStep(const QVariant& step);
+    void setBottom(const QVariant& bottom);
+    void setTop(const QVariant& top);
+
+signals:
+    void stepChanged(const QVariant& step);
+    void bottomChanged(const QVariant& bottom);
+    void topChanged(const QVariant& top);
+
+private:
+    int calculateDecimalPointPosition();
+    void createStandartValidator();
+    bool validateFixup(QString& input) const;
+
+private:
+    DECLARE_PIMPL
+};
---
+++ src/NumericalValidator.cpp
@@ -0,0 +1,277 @@
+#include <utils-qt/NumericalValidator.h>
+#include <QQmlEngine>
+#include <memory>
+#include <cmath>
+
+struct NumericalValidator::impl_t
+{
+    QVariant step;
+    QVariant rangeTop;
+    QVariant rangeBottom;
+    int decimals {0};
+
+    std::unique_ptr<QValidator> standartValidator;
+};
+
+void NumericalValidator::registerTypes(const char* url)
+{
+    qmlRegisterType<NumericalValidator>(url, 1, 0, "NumericalValidator");
+}
+
+NumericalValidator::NumericalValidator(QObject* parent)
+    : QValidator(parent)
+{
+    createImpl();
+}
+
+NumericalValidator::~NumericalValidator()
+{
+}
+
+NumericalValidator::ValueRangeStatus NumericalValidator::isValue(const QString& input)
+{
+    if (!impl().step.isValid() && !impl().rangeBottom.isValid() && !impl().rangeTop.isValid())
+        return IntermediateValue;
+    if (impl().step.type() == QVariant::Type::Double) {
+        QString top = QString::number(impl().rangeTop.toDouble());
+        QString bottom = QString::number(impl().rangeBottom.toDouble());
+        validateFixup(top);
+        validateFixup(bottom);
+        if (input == top)
+            return TopValue;
+        else if (input == bottom)
+            return BottomValue;
+        else
+            return IntermediateValue;
+    } else {
+        if (input.toInt() >= impl().rangeTop.toInt())
+            return TopValue;
+        else if (input.toInt() <= impl().rangeBottom.toInt())
+            return BottomValue;
+        else
+            return IntermediateValue;
+    }
+}
+
+QValidator::State NumericalValidator::validate(QString& input, int& pos) const
+{
+    if (input.isEmpty())
+        input += '0';
+    if (impl().step.type() == QVariant::Type::Double) {
+
+        if (input.contains(','))
+            return QValidator::State::Invalid;
+
+        if (input.startsWith('0')) {
+            int sizePrev = input.size();
+
+            if (validateFixup(input)) {
+                int sizeNow = input.size();
+                int diff = sizePrev - sizeNow;
+                pos -= diff;
+                pos = qBound(0, pos, input.size());
+            }
+        }
+
+        if (impl().step.toDouble() < 1.0) {
+            int sizePrev = input.size();
+            if (validateFixup(input)) {
+                int sizeNow = input.size();
+                int diff = sizePrev - sizeNow;
+                pos -= diff;
+                pos = qBound(0, pos, input.size());
+            }
+        }
+
+        if (input == QStringLiteral("0")) {
+            pos = 1;
+        }
+    }
+
+    if (impl().standartValidator)
+        return impl().standartValidator->validate(input, pos);
+    else
+        return State::Invalid;
+}
+
+void NumericalValidator::fixup(QString& input) const
+{
+    if (!impl().standartValidator)
+        return;
+
+    if (impl().step.type() == QVariant::Type::Int)
+        impl().standartValidator->fixup(input);
+    else
+        validateFixup(input);
+}
+
+QVariant NumericalValidator::step() const
+{
+    return impl().step;
+}
+
+QVariant NumericalValidator::bottom() const
+{
+    return impl().rangeBottom;
+}
+
+QVariant NumericalValidator::top() const
+{
+    return impl().rangeTop;
+}
+
+void NumericalValidator::setStep(const QVariant& step)
+{
+    if (step == impl().step)
+        return;
+
+    assert(step.type() == QVariant::Type::Int || step.type() == QVariant::Type::Double);
+    if (step.type() == QVariant::Type::Double)
+        assert(step.toDouble() > 0);
+
+    impl().step = step;
+    emit stepChanged(impl().step);
+    createStandartValidator();
+}
+
+void NumericalValidator::setBottom(const QVariant& bottom)
+{
+    if (bottom == impl().rangeBottom)
+        return;
+
+    assert(bottom.type() == QVariant::Type::Int || bottom.type() == QVariant::Type::Double);
+
+    impl().rangeBottom = bottom;
+    emit bottomChanged(impl().rangeBottom);
+    createStandartValidator();
+}
+
+void NumericalValidator::setTop(const QVariant& top)
+{
+    if (top == impl().rangeTop)
+        return;
+
+    assert(top.type() == QVariant::Type::Int || top.type() == QVariant::Type::Double);
+
+    impl().rangeTop = top;
+    emit topChanged(impl().rangeTop);
+    createStandartValidator();
+}
+
+int NumericalValidator::calculateDecimalPointPosition()
+{
+    auto stepDoubleFormat = impl().step.toDouble();
+    int decimalPosition = 0;
+    while (true) {
+        auto stepIntegerFormat = static_cast<unsigned int>(stepDoubleFormat);
+        stepDoubleFormat -= stepIntegerFormat;
+        if (stepDoubleFormat == 0)
+            return decimalPosition;
+
+        if (stepDoubleFormat == 10)
+            return 10;
+
+        decimalPosition++;
+        stepDoubleFormat *= 10;
+    }
+}
+
+void NumericalValidator::createStandartValidator()
+{
+    if (!impl().rangeBottom.isValid() || !impl().rangeTop.isValid() || !impl().step.isValid())
+        return;
+
+    if (step().type() == QVariant::Type::Double) {
+        impl().decimals = calculateDecimalPointPosition();
+        impl().standartValidator.reset(new QDoubleValidator(impl().rangeBottom.toDouble(), impl().rangeTop.toDouble(), impl().decimals));
+    } else {
+        impl().decimals = 0;
+        impl().standartValidator.reset(new QIntValidator(impl().rangeBottom.toInt(), impl().rangeTop.toInt()));
+    }
+}
+
+bool NumericalValidator::validateFixup(QString& input) const
+{
+    bool result = false;
+
+    if (input.back() != '.') {
+        if (input.toDouble() < impl().rangeBottom.toDouble())
+            input = QString::number(impl().rangeBottom.toDouble());
+
+        if (input.toDouble() > impl().rangeTop.toDouble())
+            input = QString::number(impl().rangeTop.toDouble());
+    }
+
+    if (input.isEmpty() || input == QStringLiteral(".")) {
+        input = QString::number(impl().rangeBottom.toDouble());
+    } else {
+        int commaDelta = 0;
+        auto it = input.begin();
+        for ( ; it != input.end(); it++) {
+            if (*it == '.') {
+                commaDelta = 1;
+                break;
+            }
+
+            if (*it != '0') break;
+        }
+
+        int count = it - input.begin();
+        int delta = (it == input.end()) ? 1 : 0;
+        int toRemove = count - delta - commaDelta;
+
+        if (toRemove) {
+            input.remove(0, toRemove);
+            result = true;
+        }
+
+        if (impl().step.toDouble() < 1.0) {
+            auto index = input.indexOf('.');
+            if (index != -1) {
+                auto comlectFractionNumber = input.split('.');
+                if (comlectFractionNumber.at(1).size() > impl().decimals) {
+                    auto fractionNumberStr = comlectFractionNumber.at(1);
+                    int digitAfterDecimals = fractionNumberStr[impl().decimals].digitValue() ;
+                    fractionNumberStr.resize(impl().decimals);
+                    int fractionNumber = fractionNumberStr.toUInt();
+                    if (digitAfterDecimals > 5) {
+                        fractionNumber++;
+                        if (fractionNumber >= std::pow(10, impl().decimals)) {
+                            QString integerNumberStr(QString::number(comlectFractionNumber.at(0).toInt()+1));
+                            fractionNumber--;
+                            fractionNumber /= std::pow(10, impl().decimals);
+                            input = integerNumberStr;
+                        } else {
+                            input = comlectFractionNumber.at(0);
+                        }
+
+                    } else {
+                        input = comlectFractionNumber.at(0);
+                    }
+
+                    if (fractionNumber != 0) {
+                        input += '.';
+                        for (;;) {
+                            if (QString::number(fractionNumber).size() <= impl().decimals)
+                                break;
+
+                            fractionNumber /= 10;
+                        }
+
+                        QString fractionNumberStr(QString::number(fractionNumber));
+
+                        while(fractionNumberStr.size() < impl().decimals) {
+                            fractionNumberStr.push_front('0');
+                        }
+
+                        input += fractionNumberStr;
+                    }
+                }
+            }
+        }
+    }
+
+    impl().standartValidator->fixup(input);
+
+    return result;
+}
