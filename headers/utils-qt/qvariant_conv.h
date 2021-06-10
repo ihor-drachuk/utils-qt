1702ad60b20b198230ab75a1dc24f7b0fc9a6560
Ihor Drachuk <ihor.drachuk@gmail.com>
2021-06-10 22:48:31 +0300

    Added QVariantConv

---
+++ headers/utils-qt/qvariant_conv.h
@@ -0,0 +1,129 @@
+#pragma once
+#include <optional>
+#include <QVariant>
+
+namespace UtilsQt {
+namespace QVariantConv {
+
+namespace Internal {
+
+//template<typename T>
+//void test(T value) {
+//    (void)value;
+//}
+
+template<int value0, int... values>
+struct Matcher
+{
+    static bool match(const QVariant& value) {
+        return (value.type() == value0) || Matcher<values...>::match(value);
+    }
+};
+
+template<int value0>
+struct Matcher<value0>
+{
+    static bool match(const QVariant& value) { /*test(value.type());*/ return value.type() == value0; }
+};
+
+template<typename T> struct TypeTools { };
+template<> struct TypeTools<int>                : Matcher<QVariant::Type::Int>        { static auto convert(const QVariant& variant, bool& ok) { return variant.toInt(&ok); } };
+template<> struct TypeTools<unsigned int>       : Matcher<QVariant::Type::UInt>       { static auto convert(const QVariant& variant, bool& ok) { return variant.toUInt(&ok); } };
+template<> struct TypeTools<long long>          : Matcher<QVariant::Type::LongLong>   { static auto convert(const QVariant& variant, bool& ok) { return variant.toLongLong(&ok); } };
+template<> struct TypeTools<unsigned long long> : Matcher<QVariant::Type::ULongLong>  { static auto convert(const QVariant& variant, bool& ok) { return variant.toULongLong(&ok); } };
+template<> struct TypeTools<double>             : Matcher<QVariant::Type::Double>     { static auto convert(const QVariant& variant, bool& ok) { return variant.toDouble(&ok); } };
+template<> struct TypeTools<float>              : Matcher<QMetaType::Type::Float>     { static auto convert(const QVariant& variant, bool& ok) { return variant.toFloat(&ok); } };
+template<> struct TypeTools<bool>               : Matcher<QVariant::Type::Bool>       { static auto convert(const QVariant& variant, bool& ok) { ok = true; return variant.toBool(); } };
+template<> struct TypeTools<char>               : Matcher<QVariant::Type::Int>        { static auto convert(const QVariant& variant, bool& ok) { auto rsl = variant.toInt(&ok); ok &= (rsl >= 0 && rsl <= 255); return rsl; } };
+template<> struct TypeTools<QChar>              : Matcher<QVariant::Type::Char>       { static auto convert(const QVariant& variant, bool& ok) { ok = true; return variant.toChar(); } };
+template<> struct TypeTools<QString>            : Matcher<QVariant::Type::String>     { static auto convert(const QVariant& variant, bool& ok) { ok = true; return variant.toString(); } };
+
+} // namespace Internal
+
+enum Checks
+{
+    NoCheck = 0,
+
+    Check_NonNull_Valid = 1,
+    Check_Type = 2,
+    Check_ConvResult = 4,
+
+    CheckAll = Check_NonNull_Valid |
+               Check_Type |
+               Check_ConvResult
+};
+
+template<typename T,
+         typename std::enable_if<!std::is_enum<T>::value>::type* = nullptr>
+bool load(const QVariant& src, T& dst, Checks checks = CheckAll)
+{
+    if (checks & Check_NonNull_Valid)
+        if (src.isNull() || !src.isValid())
+            return false;
+
+    if (checks & Check_Type)
+        if (!Internal::TypeTools<T>::match(src))
+            return false;
+
+    bool ok = false;
+    T result = Internal::TypeTools<T>::convert(src, ok);
+
+    if ((checks & Check_ConvResult) == 0)
+        ok = true;
+
+    if (ok)
+        dst = result;
+
+    return ok;
+}
+
+template<typename T,
+         typename std::enable_if<std::is_enum<T>::value>::type* = nullptr>
+bool load(const QVariant& src, T& dst, Checks checks = CheckAll)
+{
+    int value;
+    auto ok = load(src, value, checks);
+
+    if (ok)
+        dst = static_cast<T>(value);
+
+    return ok;
+}
+
+template<typename T>
+std::optional<T> load(const QVariant& src, Checks checks = CheckAll)
+{
+    T result;
+    bool ok = load(src, result, checks);
+    return ok ? std::optional<T>(result) : std::nullopt;
+}
+
+} // namespace QVariantConv
+} // namespace UtilsQt
+
+inline UtilsQt::QVariantConv::Checks operator | (UtilsQt::QVariantConv::Checks a, UtilsQt::QVariantConv::Checks b)
+{
+    return (UtilsQt::QVariantConv::Checks)((int)a | (int)b);
+}
+
+inline UtilsQt::QVariantConv::Checks& operator |= (UtilsQt::QVariantConv::Checks& a, UtilsQt::QVariantConv::Checks b)
+{
+    (int&)a |= (int)b;
+    return a;
+}
+
+inline UtilsQt::QVariantConv::Checks operator & (UtilsQt::QVariantConv::Checks a, UtilsQt::QVariantConv::Checks b)
+{
+    return (UtilsQt::QVariantConv::Checks)((int)a & (int)b);
+}
+
+inline UtilsQt::QVariantConv::Checks& operator &= (UtilsQt::QVariantConv::Checks& a, UtilsQt::QVariantConv::Checks b)
+{
+    (int&)a &= (int)b;
+    return a;
+}
+
+inline UtilsQt::QVariantConv::Checks operator ~ (UtilsQt::QVariantConv::Checks a)
+{
+    return (UtilsQt::QVariantConv::Checks)(~(int)a & (int)(UtilsQt::QVariantConv::Checks::CheckAll));
+}
---
+++ tests/test/test6_qvariant_conv.cpp
@@ -0,0 +1,42 @@
+#include <gtest/gtest.h>
+#include <utils-qt/qvariant_conv.h>
+
+using namespace UtilsQt::QVariantConv;
+
+TEST(UtilsQt, QVariantConvTest_Operators)
+{
+    ASSERT_EQ((Checks)1+2+4 & (Checks)2, (Checks)2);
+    ASSERT_EQ((Checks)1+4 | (Checks)2, (Checks)1+2+4);
+
+    Checks a = NoCheck;
+    a |= Checks::Check_NonNull_Valid;
+    a |= Checks::Check_Type;
+    ASSERT_EQ(a, Check_NonNull_Valid | Check_Type);
+
+    a &= Check_Type;
+    ASSERT_EQ(a, Check_Type);
+}
+
+TEST(UtilsQt, QVariantConvTest_Load)
+{
+    ASSERT_EQ(load<int>(QVariant(170)).value(), 170);
+    ASSERT_EQ(load<unsigned int>(QVariant(170U)).value(), 170);
+    ASSERT_EQ(load<long long>(QVariant(170LL)).value(), 170);
+    ASSERT_EQ(load<unsigned long long>(QVariant(170ULL)).value(), 170);
+
+    ASSERT_DOUBLE_EQ(load<double>(QVariant((double)170.2)).value(), 170.2);
+    ASSERT_FLOAT_EQ(load<float>(QVariant((float)170.2)).value(), 170.2);
+
+    ASSERT_EQ(load<bool>(QVariant(true)).value(), true);
+    ASSERT_EQ(load<bool>(QVariant(false)).value(), false);
+
+    ASSERT_EQ(load<char>(QVariant('a')).value(), 'a');
+    ASSERT_EQ(load<QChar>(QVariant(QChar('a'))).value(), 'a');
+    ASSERT_EQ(load<QString>(QVariant("str")).value(), "str");
+
+
+    ASSERT_DOUBLE_EQ(load<double>(QVariant("170.2"), ~Check_Type).value(), 170.2);
+
+    ASSERT_EQ(load<QString>(QVariant(), NoCheck).value(), "");
+    ASSERT_EQ(load<int>(QVariant(), NoCheck).value(), 0);
+}
