0adad54773deb871e31927791693b704d5ad1027
Ihor Drachuk <ihor.drachuk@gmail.com>
2021-07-08 00:23:28 +0300

    ListModelTools: added signals, getters, tests

--- headers/utils-qt/qml-cpp/ListModelTools.h
+++ headers/utils-qt/qml-cpp/ListModelTools.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <QAbstractListModel>
+#include <QJSValue>
 #include <utils-cpp/pimpl.h>
 
 class ListModelTools : public QObject
@@ -7,6 +8,9 @@ class ListModelTools : public QObject
     Q_OBJECT
 public:
     Q_PROPERTY(QAbstractListModel* model READ model WRITE setModel NOTIFY modelChanged)
+    Q_PROPERTY(QStringList roles READ roles /*WRITE setRoles*/ NOTIFY rolesChanged)
+    Q_PROPERTY(bool allowJsValues READ allowJsValues WRITE setAllowJsValues NOTIFY allowJsValuesChanged)
+    Q_PROPERTY(bool allowRoles READ allowRoles WRITE setAllowRoles NOTIFY allowRolesChanged)
     Q_PROPERTY(int itemsCount READ itemsCount /*WRITE setItemsCount*/ NOTIFY itemsCountChanged)
 
     static void registerTypes(const char* url);
@@ -14,25 +18,56 @@ public:
     explicit ListModelTools(QObject* parent = nullptr);
     ~ListModelTools() override;
 
+    Q_INVOKABLE QVariant getData(int index, const QString& role = {}) const;
+    Q_INVOKABLE QVariantMap getDataByRoles(int index, const QStringList& roles) const;
+
+signals:
+    void beforeModelReset();
+    void modelReset();
+    void beforeInserted(int index1, int index2);
+    void inserted(int index1, int index2);
+    void beforeRemoved(int index1, int index2, QJSValue tester);
+    void removed(int index1, int index2, QJSValue tester);
+    void changed(int index1, int index2, QJSValue tester,
+                 const QStringList& affectedRoles);
+
 // --- Properties support ---
 public:
     QAbstractListModel* model() const;
+    bool allowJsValues() const;
     int itemsCount() const;
+    bool allowRoles() const;
+    QStringList roles() const;
+
 public slots:
     void setModel(QAbstractListModel* value);
+    void setAllowJsValues(bool value);
     void setItemsCount(int value);
+    void setAllowRoles(bool value);
+    void setRoles(QStringList value);
+
 signals:
     void modelChanged(QAbstractListModel* model);
+    void allowJsValuesChanged(bool allowJsValues);
     void itemsCountChanged(int itemsCount);
+    void allowRolesChanged(bool allowRoles);
+    void rolesChanged(const QStringList& roles);
 // --- ---
 
 private:
+    QJSValue createTester(int low, int high);
     void updateItemsCount();
+    QStringList listRoles() const;
+    void fillRolesMap();
 
     // From model
+    void onBeforeRowsInserted(const QModelIndex& parent, int first, int last);
     void onRowsInserted(const QModelIndex& parent, int first, int last);
+    void onBeforeRowsRemoved(const QModelIndex& parent, int first, int last);
     void onRowsRemoved(const QModelIndex& parent, int first, int last);
+    void onBeforeModelReset();
     void onModelReset();
+    void onDataChanged(const QModelIndex& topLeft, const QModelIndex& bottomRight, const QVector<int>& roles = QVector<int>());
 
 private:
     DECLARE_PIMPL
--- src/qml-cpp/ListModelTools.cpp
+++ src/qml-cpp/ListModelTools.cpp
@@ -1,11 +1,16 @@
 #include "utils-qt/qml-cpp/ListModelTools.h"
 
 #include <QQmlEngine>
-
+#include <QQmlContext>
+#include <QMap>
 
 struct ListModelTools::impl_t
 {
     QAbstractListModel* model { nullptr };
+    QStringList roles;
+    QMap<QString, int> rolesMap;
+    bool allowJsValues { false };
+    bool allowRoles { false };
     int itemsCount { 0 };
 };
 
@@ -27,6 +32,45 @@ ListModelTools::~ListModelTools()
 {
 }
 
+QVariant ListModelTools::getData(int index, const QString& role) const
+{
+    if (role.isEmpty()) {
+        return getDataByRoles(index, impl().roles);
+    } else {
+        auto v = getDataByRoles(index, {role});
+
+        if (v.isEmpty())
+            return v;
+
+        return v.value(role);
+    }
+}
+
+QVariantMap ListModelTools::getDataByRoles(int index, const QStringList& roles) const
+{
+    if (!impl().model)
+        return {};
+
+    assert(index >= 0 && index < impl().model->rowCount());
+    if (!(index >= 0 && index < impl().model->rowCount())) {
+        return {};
+    }
+
+    const QStringList& rolesRef = roles.isEmpty() ?
+                                      impl().roles :
+                                      roles;
+
+    QVariantMap result;
+    auto it = rolesRef.cbegin();
+    const auto itEnd = rolesRef.cend();
+    while (it != itEnd) {
+        result[*it] = impl().model->data(impl().model->index(index), impl().rolesMap.value(*it, -1));
+        it++;
+    }
+
+    return result;
+}
+
 QAbstractListModel* ListModelTools::model() const
 {
     return impl().model;
@@ -35,6 +79,21 @@ QAbstractListModel* ListModelTools::model() const
 int ListModelTools::itemsCount() const
 {
     return impl().itemsCount;
+}
+
+bool ListModelTools::allowRoles() const
+{
+    return impl().allowRoles;
+}
+
+QStringList ListModelTools::roles() const
+{
+    return impl().roles;
+}
+
+bool ListModelTools::allowJsValues() const
+{
+    return impl().allowJsValues;
 }
 
 void ListModelTools::setModel(QAbstractListModel* value)
@@ -51,15 +110,25 @@ void ListModelTools::setModel(QAbstractListModel* value)
     }
 
     impl().model = value;
-    emit modelChanged(impl().model);
+    impl().itemsCount = impl().model ? impl().model->rowCount() : 0;
+    fillRolesMap();
 
     if (impl().model) {
-        QObject::connect(impl().model, &QAbstractListModel::rowsInserted, this, &ListModelTools::onRowsInserted);
-        QObject::connect(impl().model, &QAbstractListModel::rowsRemoved, this, &ListModelTools::onRowsRemoved);
+        QObject::connect(impl().model, &QAbstractListModel::modelAboutToBeReset, this, &ListModelTools::onBeforeModelReset);
         QObject::connect(impl().model, &QAbstractListModel::modelReset, this, &ListModelTools::onModelReset);
+
+        QObject::connect(impl().model, &QAbstractListModel::rowsAboutToBeInserted, this, &ListModelTools::onBeforeRowsInserted);
+        QObject::connect(impl().model, &QAbstractListModel::rowsInserted, this, &ListModelTools::onRowsInserted);
+
+        QObject::connect(impl().model, &QAbstractListModel::rowsAboutToBeRemoved, this, &ListModelTools::onBeforeRowsRemoved);
+        QObject::connect(impl().model, &QAbstractListModel::rowsRemoved, this, &ListModelTools::onRowsRemoved);
+
+        QObject::connect(impl().model, &QAbstractListModel::dataChanged, this, &ListModelTools::onDataChanged);
     }
 
-    updateItemsCount();
+    emit modelChanged(impl().model);
+    emit itemsCountChanged(impl().itemsCount);
+    emit rolesChanged(impl().roles);
 }
 
 void ListModelTools::setItemsCount(int value)
@@ -71,22 +140,143 @@ void ListModelTools::setItemsCount(int value)
     emit itemsCountChanged(impl().itemsCount);
 }
 
+void ListModelTools::setAllowRoles(bool value)
+{
+    if (impl().allowRoles == value)
+        return;
+
+    impl().allowRoles = value;
+    emit allowRolesChanged(impl().allowRoles);
+}
+
+void ListModelTools::setRoles(QStringList value)
+{
+    if (impl().roles == value)
+        return;
+
+    impl().roles = value;
+    emit rolesChanged(impl().roles);
+}
+
+void ListModelTools::setAllowJsValues(bool value)
+{
+    if (impl().allowJsValues == value)
+        return;
+
+    impl().allowJsValues = value;
+    emit allowJsValuesChanged(impl().allowJsValues);
+}
+
+QJSValue ListModelTools::createTester(int low, int high)
+{
+    if (impl().allowJsValues) {
+        auto ctx = QQmlEngine::contextForObject(this);
+        auto engine = ctx->engine();
+        engine->evaluate(QString("( function(exports) {"
+            "exports.tester = function(index) { return (index >= %1) && (index <= %2); };"
+        "})(this.listModelTools = {})").arg(low).arg(high));
+        auto f = engine->globalObject().property("listModelTools").property("tester");
+        return f;
+    } else {
+        return QJSValue(QJSValue::SpecialValue::NullValue);
+    }
+}
+
 void ListModelTools::updateItemsCount()
 {
     setItemsCount(impl().model ? impl().model->rowCount() : 0);
 }
 
-void ListModelTools::onRowsInserted(const QModelIndex& /*parent*/, int /*first*/, int /*last*/)
+QStringList ListModelTools::listRoles() const
 {
-    updateItemsCount();
+    if (!impl().model)
+        return {};
+
+    QStringList result;
+    auto roles = impl().model->roleNames();
+    for (const auto& x : qAsConst(roles)) {
+        result.append(QString::fromLatin1(x));
+    }
+
+    return result;
 }
 
-void ListModelTools::onRowsRemoved(const QModelIndex& /*parent*/, int /*first*/, int /*last*/)
+void ListModelTools::fillRolesMap()
+{
+    impl().rolesMap.clear();
+    impl().roles.clear();
+
+    if (!impl().model)
+        return;
+
+    auto roles = impl().model->roleNames();
+    auto it = roles.cbegin();
+    const auto itEnd = roles.cend();
+    while (it != itEnd) {
+        auto value = QString::fromLatin1(it.value());
+        impl().rolesMap.insert(value, it.key());
+        impl().roles.append(value);
+        it++;
+    }
+}
+
+void ListModelTools::onBeforeRowsInserted(const QModelIndex& /*parent*/, int first, int last)
+{
+    emit beforeInserted(first, last);
+}
+
+void ListModelTools::onRowsInserted(const QModelIndex& /*parent*/, int first, int last)
 {
     updateItemsCount();
+    emit inserted(first, last);
+}
+
+void ListModelTools::onBeforeRowsRemoved(const QModelIndex& /*parent*/, int first, int last)
+{
+    emit beforeRemoved(first, last, createTester(first, last));
+}
+
+void ListModelTools::onRowsRemoved(const QModelIndex& /*parent*/, int first, int last)
+{
+    updateItemsCount();
+    emit removed(first, last, createTester(first, last));
+}
+
+void ListModelTools::onBeforeModelReset()
+{
+    emit beforeModelReset();
 }
 
 void ListModelTools::onModelReset()
 {
     updateItemsCount();
+    emit modelReset();
+}
+
+void ListModelTools::onDataChanged(const QModelIndex& topLeft, const QModelIndex& bottomRight, const QVector<int>& roles)
+{
+    auto first = topLeft.row();
+    auto last = bottomRight.row();
+    auto tester = createTester(first, last);
+    QStringList roleNames;
+
+    if (impl().allowRoles) {
+        auto modelRoles = impl().model->roleNames();
+
+        if (roles.isEmpty()) {
+            for (const auto& x : qAsConst(modelRoles)) {
+                roleNames.append(QString::fromLatin1(x));
+            }
+        } else {
+            auto it = modelRoles.begin();
+            const auto itEnd = modelRoles.end();
+            while (it != itEnd) {
+                if (roles.contains(it.key()))
+                    roleNames.append(QString::fromLatin1(it.value()));
+                it++;
+            }
+        }
+    }
+
+    emit changed(first, last, tester, roleNames);
 }
