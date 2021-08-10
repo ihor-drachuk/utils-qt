19ef760081d422d9ab3bd30579d2428969d8e480
Ihor Drachuk <ihor.drachuk@gmail.com>
2021-08-10 19:43:01 +0300

    Better struct_ops2_qhash.h

--- headers/utils-qt/struct_ops2_qhash.h
+++ headers/utils-qt/struct_ops2_qhash.h
@@ -1,8 +1,21 @@
 #pragma once
 #include <QtCore>
 #include <QHash>
-#include <utils-cpp/tuple_for_each.h>
 
+/*
+    Example
+  -----------
+
+  struct My_Struct
+  {
+      int a_;
+      double b_;
+
+      auto tie() const { return std::tie(a_, b_); }
+  };
+
+  STRUCT_QHASH(My_Struct);
+*/
 
 namespace struct_ops_internal {
 
@@ -11,35 +24,36 @@ inline uint combineHash(uint hash, uint seed)
     return seed ^ (hash + 0x9e3779b9 + (seed << 6) + (seed >> 2)) ;
 }
 
-template<typename StructTuple>
-inline uint hashTuple(const StructTuple& tuple)
+template<std::size_t I = 0, typename... Tp>
+inline typename std::enable_if<I == sizeof...(Tp) - 1, uint>::type
+hash(const std::tuple<Tp...>& t, uint seed)
 {
-    auto result = for_each_par<uint>(tuple, [](int, const auto& v, const std::optional<uint>& param) -> std::optional<uint> {
-        uint hash1 = *param;
-        uint hash2 = qHash(v);
-        return combineHash(hash1, hash2);
-    }, 0, false);
+    return combineHash(qHash(std::get<I>(t)), seed);
+}
 
-    return *result;
+template<std::size_t I = 0, typename... Tp>
+inline typename std::enable_if<I < sizeof...(Tp) - 1, uint>::type
+hash(const std::tuple<Tp...>& t, uint seed)
+{
+    return combineHash(qHash(std::get<I>(t)), hash<I+1>(t, seed));
+}
+
+inline uint hash(const std::tuple<>&, uint seed)
+{
+    return combineHash(0, seed);
+}
+
+template<typename... Tp>
+inline uint hashTuple(const std::tuple<Tp...>& value, uint seed)
+{
+    return hash(value, seed);
 }
 
 } // namespace struct_ops_internal
 
 
-#define TIED_QHASH(STRUCT) \
-    inline uint qHash(const STRUCT& s) \
+#define STRUCT_QHASH(STRUCT) \
+    inline uint qHash(const STRUCT& s, uint seed = 0) \
     { \
-        return struct_ops_internal::hashTuple(s.tie()); \
+        return struct_ops_internal::hashTuple(s.tie(), seed); \
     }
-
-//    Example
-//  -----------
-//
-//  struct My_Struct
-//  {
-//      int a_;
-//      double b_;
-//      auto tie() const { return std::tie(a_, b_); }
-//  };
-//
-//  TIED_QHASH(My_Struct)
