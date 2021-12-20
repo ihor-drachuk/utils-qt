4d275a27877b62b5bc5a71590a464262c12a0fb9
k0p4 <alexander.kopnin@developex.net>
2021-12-20 15:11:52 +0200

    Added 'count' to ListModelItemProxy + fixes to emits order

--- headers/utils-qt/qml-cpp/ListModelItemProxy.h
+++ headers/utils-qt/qml-cpp/ListModelItemProxy.h
@@ -9,6 +9,7 @@ class ListModelItemProxy : public QObject
     Q_OBJECT
 public:
     Q_PROPERTY(QAbstractListModel* model READ model WRITE setModel NOTIFY modelChanged)
+    Q_PROPERTY(int count READ count /*WRITE setCount*/ NOTIFY countChanged)
     Q_PROPERTY(int index READ index WRITE setIndex NOTIFY indexChanged)
     Q_PROPERTY(bool keepIndexTrack READ keepIndexTrack WRITE setKeepIndexTrack NOTIFY keepIndexTrackChanged)
 
@@ -28,6 +29,7 @@ signals:
 // --- Properties support ---
 public:
     QAbstractListModel* model() const;
+    int count() const;
     int index() const;
     bool ready() const;
     QQmlPropertyMap* propertyMap() const;
@@ -44,6 +46,7 @@ private slots:
 
 signals:
     void modelChanged(QAbstractListModel* model);
+    void countChanged(int);
     void indexChanged(int index);
     void readyChanged(bool ready);
     void propertyMapChanged(QQmlPropertyMap* propertyMap);
