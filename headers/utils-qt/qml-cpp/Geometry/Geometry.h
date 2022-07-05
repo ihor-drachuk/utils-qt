0df62a15f4285221d6bd4bcfa33d4bedbf24c942
Ihor Drachuk <ihor.drachuk@gmail.com>
2022-07-05 20:09:10 +0300

    Extended Geometry functionality

--- headers/utils-qt/qml-cpp/Geometry/Geometry.h
+++ headers/utils-qt/qml-cpp/Geometry/Geometry.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <QObject>
+#include <QPolygonF>
+#include <QVector>
 #include <utils-cpp/pimpl.h>
 
 class Geometry : public QObject
@@ -8,6 +10,11 @@ class Geometry : public QObject
 public:
     static Geometry& instance();
     static void registerTypes();
+
+    Q_INVOKABLE [[nodiscard]] QPolygonF polygonScale(const QPolygonF& polygon, qreal factor) const;
+    Q_INVOKABLE [[nodiscard]] QVector<QPolygonF> polygonsScale(const QVector<QPolygonF>& polygons, qreal factor) const;
+    Q_INVOKABLE void polygonScaleRef(QPolygonF& polygon, qreal factor) const;
+    Q_INVOKABLE void polygonsScaleRef(QVector<QPolygonF>& polygons, qreal factor) const;
 
 // --- Properties support ---
 public:
