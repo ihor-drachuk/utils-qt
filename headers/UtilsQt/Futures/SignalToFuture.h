10e9d9de59366f72e22fbb0c28c2e0942ada6133
Ihor Drachuk <ihor.drachuk@gmail.com>
2023-09-20 22:59:54 +0300

    Futures: added SignalToFuture

---
+++ headers/UtilsQt/Futures/SignalToFuture.h
@@ -0,0 +1,72 @@
+#pragma once
+#include <UtilsQt/Futures/Utils.h>
+#include <QObject>
+#include <tuple>
+#include <type_traits>
+
+/*  Overview
+ *
+ *  There is function which creates one-time QFuture for specified signal.
+ *  Usage examples:
+ *    auto f = signalToFuture(&obj, &Class::someSignal);
+ *    auto f = signalToFuture(&obj, &Class::someSignal, this); // With lifetime context
+ *
+ *  Return type depends on signal parameters count and types:
+ *    0  - QFuture<void>
+ *    1  - QFuture<T>
+ *    2+ - QFuture<std::tuple<Args...>>
+ */
+
+namespace UtilsQt {
+
+namespace Internal {
+
+template<size_t Count, typename... Args>
+struct ResultProviderImpl {
+    using ReturnType = std::tuple<std::remove_cv_t<std::remove_reference_t<Args>>...>;
+
+    static void apply(Promise<ReturnType>& promise, const Args&... args) { promise.finish({args...}); }
+};
+
+template<typename T>
+struct ResultProviderImpl<1, T> {
+    using ReturnType = std::remove_cv_t<std::remove_reference_t<T>>;
+    static void apply(Promise<ReturnType>& promise, const T& arg) { promise.finish(arg); }
+};
+
+template<>
+struct ResultProviderImpl<0> {
+    using ReturnType = void;
+    static void apply(Promise<ReturnType>& promise) { promise.finish(); }
+};
+
+template<typename... Args>
+struct ResultProvider : public ResultProviderImpl<sizeof...(Args), Args...> { };
+
+
+} // namespace Internal
+
+template<typename Object, typename... Args,
+         typename ResultType = typename Internal::ResultProvider<Args...>::ReturnType,
+         typename std::enable_if<std::is_base_of<QObject, Object>::value>::type* = nullptr>
+QFuture<ResultType> signalToFuture(Object* object, void (Object::* signal)(Args...), QObject* context = nullptr)
+{
+    auto promise = UtilsQt::createPromise<ResultType>();
+
+    auto ctx = new QObject();
+    QObject::connect(object, &QObject::destroyed, ctx, &QObject::deleteLater);
+    if (context) QObject::connect(context, &QObject::destroyed, ctx, &QObject::deleteLater);
+
+    QObject::connect(object, signal, ctx, [promise, ctx](Args... args) mutable {
+        if (!promise.isFinished()) Internal::ResultProvider<Args...>::apply(promise, args...);
+        ctx->deleteLater();
+    });
+
+    QObject::connect(ctx, &QObject::destroyed, [promise]() mutable {
+        if (!promise.isFinished()) promise.cancel();
+    });
+
+    return promise.future();
+}
+
+} // namespace UtilsQt
---
+++ tests/test/Test_Futures_5_SignalToFuture.cpp
@@ -0,0 +1,120 @@
+#include <gtest/gtest.h>
+#include <UtilsQt/Futures/SignalToFuture.h>
+#include <QCoreApplication>
+#include <QEventLoop>
+#include <QString>
+
+class Futures_SignalToFuture_TestClass : public QObject
+{
+    Q_OBJECT
+public:
+    void trigger1() { emit someSignal1(); }
+    void trigger2() { emit someSignal2("Test"); }
+    void trigger3() { emit someSignal3("Test", 12); }
+
+signals:
+    void someSignal1();
+    void someSignal2(const QString& text);
+    void someSignal3(const QString& text, int number);
+};
+
+using namespace UtilsQt;
+
+TEST(UtilsQt, Futures_SignalToFuture_Basic)
+{
+    using Class = Futures_SignalToFuture_TestClass;
+
+    Class obj;
+    auto f = signalToFuture(&obj, &Class::someSignal1);
+
+    qApp->processEvents();
+    qApp->processEvents();
+    ASSERT_FALSE(f.isFinished());
+    ASSERT_FALSE(f.isCanceled());
+
+    obj.trigger2();
+    obj.trigger3();
+    qApp->processEvents();
+    qApp->processEvents();
+    ASSERT_FALSE(f.isFinished());
+    ASSERT_FALSE(f.isCanceled());
+
+    obj.trigger1();
+    ASSERT_TRUE(f.isFinished());
+    ASSERT_FALSE(f.isCanceled());
+
+    auto f2 = signalToFuture(&obj, &Class::someSignal2);
+    auto f3 = signalToFuture(&obj, &Class::someSignal3);
+    obj.trigger2();
+    obj.trigger3();
+    ASSERT_TRUE(f2.isFinished());
+    ASSERT_TRUE(f3.isFinished());
+    ASSERT_FALSE(f2.isCanceled());
+    ASSERT_FALSE(f3.isCanceled());
+}
+
+
+TEST(UtilsQt, Futures_SignalToFuture_Context)
+{
+    using Class = Futures_SignalToFuture_TestClass;
+
+    // Main context (source object)
+    auto obj = std::make_shared<Class>();
+    auto f = signalToFuture(obj.get(), &Class::someSignal1);
+
+    obj.reset();
+    waitForFuture<QEventLoop>(f);
+    ASSERT_TRUE(f.isFinished());
+    ASSERT_TRUE(f.isCanceled());
+
+    // Usual context
+    obj = std::make_shared<Class>();
+    auto obj2 = std::make_shared<QObject>();
+    f = signalToFuture(obj.get(), &Class::someSignal1, obj2.get());
+    ASSERT_FALSE(f.isFinished());
+    ASSERT_FALSE(f.isCanceled());
+
+    obj2.reset();
+    waitForFuture<QEventLoop>(f);
+    ASSERT_TRUE(f.isFinished());
+    ASSERT_TRUE(f.isCanceled());
+}
+
+TEST(UtilsQt, Futures_SignalToFuture_ReturnedValue)
+{
+    using Class = Futures_SignalToFuture_TestClass;
+
+    Class obj;
+    auto f1 = signalToFuture(&obj, &Class::someSignal1);
+    auto f2 = signalToFuture(&obj, &Class::someSignal2);
+    auto f3 = signalToFuture(&obj, &Class::someSignal3);
+
+    static_assert (std::is_same_v<decltype(f1), QFuture<void>>, "Wrong return type for case 1");
+    static_assert (std::is_same_v<decltype(f2), QFuture<QString>>, "Wrong return type for case 2");
+    static_assert (std::is_same_v<decltype(f3), QFuture<std::tuple<QString, int>>>, "Wrong return type for case 3");
+
+    obj.trigger1();
+    obj.trigger2();
+    obj.trigger3();
+
+    waitForFuture<QEventLoop>(f1);
+    waitForFuture<QEventLoop>(f2);
+    waitForFuture<QEventLoop>(f3);
+
+    ASSERT_TRUE(f1.isFinished());
+    ASSERT_TRUE(f2.isFinished());
+    ASSERT_TRUE(f3.isFinished());
+
+    ASSERT_FALSE(f1.isCanceled());
+    ASSERT_FALSE(f2.isCanceled());
+    ASSERT_FALSE(f3.isCanceled());
+
+    // Skip f1 (void)
+    const auto r2 = f2.result();
+    const auto r3 = f3.result();
+
+    ASSERT_EQ(r2, "Test");
+    ASSERT_EQ(r3, std::make_tuple(QString("Test"), 12));
+}
+
+#include "Test_Futures_5_SignalToFuture.moc"
