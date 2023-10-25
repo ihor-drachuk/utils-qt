3c8a24098d7aae797a9e9469d258b89adf1748eb
Ihor Drachuk <ihor.drachuk@gmail.com>
2023-10-25 20:32:41 +0300

    MLM custom resetters: tests added

---
+++ tests/test4_qml_auto/MLMResetter.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <QObject>
+#include <QQmlEngine>
+#include <utils-cpp/copy_move.h>
+#include <UtilsQt/mergedlistmodel.h>
+
+class MLMResetter : public QObject
+{
+    Q_OBJECT
+    NO_COPY_MOVE(MLMResetter);
+
+public:
+    static void registerTypes()
+    {
+        qmlRegisterSingletonType<MLMResetter>("UtilsQt", 1, 0, "MLMResetter", [](QQmlEngine*, QJSEngine*) -> QObject* {
+            return new MLMResetter();
+        });
+    }
+
+    explicit MLMResetter(QObject* parent = nullptr)
+        : QObject(parent)
+    { }
+
+    Q_INVOKABLE void setCustomResetter(MergedListModel* mlm)
+    {
+        mlm->registerCustomResetter("interest", [](int /*role*/, const QLatin1String& /*roleStr*/, int /*row*/, const QVariant& /*prevValue*/){
+            return "I-DELETED";
+        });
+
+        mlm->registerCustomResetter([](int /*role*/, const QLatin1String& /*roleStr*/, int /*row*/, const QVariant& /*prevValue*/){
+            return "DELETED";
+        });
+    }
+};
