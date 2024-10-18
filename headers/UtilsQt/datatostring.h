9ee6b9dd94327494454340d46dda5d4098187988
Ihor Drachuk <ihor-drachuk-libs@pm.me>
2024-10-18 13:54:19 +0300

    Moved 'dataToString' to utils_cpp library (as data_to_string)

--- headers/UtilsQt/datatostring.h
+++
@@ -1,8 +0,0 @@
-/* License:  MIT
- * Source:   https://github.com/ihor-drachuk/utils-qt
- * Contact:  ihor-drachuk-libs@pm.me  */
-
-#pragma once
-#include <QString>
-
-QString dataToString(const void* data, size_t sz);
--- tests/benchmark/utils_qt.cpp
+++ tests/benchmark/utils_qt.cpp
@@ -4,8 +4,6 @@
 
 #include <benchmark/benchmark.h>
 
-#include <UtilsQt/datatostring.h>
-
 static void StubBenchmark(benchmark::State& state)
 {
     int i = 0;
@@ -17,25 +15,5 @@ static void StubBenchmark(benchmark::State& state)
 }
 
 BENCHMARK(StubBenchmark);
-
-
-static void Benchmark_dataToString(benchmark::State& state)
-{
-    std::string someData = "1234My567Data12";
-    someData[0] = 1;
-    someData[1] = 2;
-    someData[2] = 3;
-    someData[3] = 4;
-    someData[6] = 5;
-    someData[7] = 6;
-    someData[8] = 7;
-    someData[13] = 8;
-    someData[14] = 9;
-
-    while (state.KeepRunning())
-        dataToString(someData.data(), someData.size());
-}
-
-BENCHMARK(Benchmark_dataToString);
 
 BENCHMARK_MAIN();
